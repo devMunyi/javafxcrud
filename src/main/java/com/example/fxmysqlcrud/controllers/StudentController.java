c800686631c4d6d7a862819f13a8870aeb292c72
devMunyi <samunyi90@gmail.com>
2023-07-05 11:28:37 +0300

    Add Delete, Update and New Actions

--- src/main/java/com/example/fxmysqlcrud/controllers/StudentController.java
+++ src/main/java/com/example/fxmysqlcrud/controllers/StudentController.java
@@ -2,11 +2,13 @@ package com.example.fxmysqlcrud.controllers;
 
 import com.example.fxmysqlcrud.data.AppQuery;
 import com.example.fxmysqlcrud.models.Student;
-import javafx.beans.Observable;
+
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.Objects;
@@ -19,32 +21,47 @@ public class StudentController implements Initializable {
 
     public TableColumn<Student, Integer> colUid;
     public TableColumn<Student, String> colFirstname, colMiddleName, colLastname;
+
+    private Student studentInfoCopy;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        btnSave.setOnAction(event -> onAddStudent());
         initTableData();
+        btnSave.setOnAction(this::onAddStudent);
+        tableView.setOnMouseClicked(this::tblRowMouseClick);
+        btnUpdate.setOnAction(this::onUpdateStudent);
+        btnDelete.setOnAction(this::onDeleteStudent);
+        btnNew.setOnAction(this::onNewStudent);
+        btnUpdate.setDisable(true);
+        btnDelete.setDisable(true);
     }
 
     private void initTableData(){
         AppQuery aq = new AppQuery();
         ObservableList<Student> list = aq.getStudentList();
-        colUid.setCellValueFactory(new PropertyValueFactory<Student, Integer>("uid"));
-        colFirstname.setCellValueFactory(new PropertyValueFactory<Student, String>("firstname"));
-        colMiddleName.setCellValueFactory(new PropertyValueFactory<Student, String>("middlename"));
-        colLastname.setCellValueFactory(new PropertyValueFactory<Student, String>("lastname"));
+        colUid.setCellValueFactory(new PropertyValueFactory<>("uid"));
+        colFirstname.setCellValueFactory(new PropertyValueFactory<>("firstname"));
+        colMiddleName.setCellValueFactory(new PropertyValueFactory<>("middlename"));
+        colLastname.setCellValueFactory(new PropertyValueFactory<>("lastname"));
         tableView.setItems(list);
     }
 
-    private void onAddStudent(){
+    public void onNewStudent(ActionEvent actionEvent){
+        disableDelAndUpdateBtn();
+        btnSave.setDisable(false);
+        emptyFields();
+    }
+
+    public void onAddStudent(ActionEvent actionEvent){
+
         // get user inputs form text fields
         String firstname = tfFirstname.getText() != null ? tfFirstname.getText().trim() : "";
         String middleName = tfMiddleName.getText() != null ? tfMiddleName.getText().trim() : "";
         String lastname = tfLastname.getText() != null ? tfLastname.getText().trim() : "";
 
         // validations fields are not empty
-        if(noInputValidation("First Name", firstname)){return;};
-        if(noInputValidation("Middle Name", middleName)){return;};
-        if(noInputValidation("Last Name", lastname)){return;};
+        if(noInput("First Name", firstname)){return;}
+        if(noInput("Middle Name", middleName)){return;}
+        if(noInput("Last Name", lastname)){return;}
 
 
         Student student = new Student(firstname, middleName, lastname);
@@ -53,9 +70,12 @@ public class StudentController implements Initializable {
         aq.addStudent(student); // add new student
         emptyFields(); // empty fields
         toastAlert("Success", "Saved Successfully!");
+
+        // refresh table list
+        initTableData();
     }
 
-    private boolean noInputValidation(String fieldName, String fieldContent){
+    private boolean noInput(String fieldName, String fieldContent){
         boolean yes = false;
         if(fieldContent.isEmpty()){
             yes = true;
@@ -81,5 +101,91 @@ public class StudentController implements Initializable {
         tfFirstname.setText("");
         tfMiddleName.setText("");
         tfLastname.setText("");
+    }
+
+    private void tblRowMouseClick(MouseEvent mouseEvent) {
+        Student student = tableView.getSelectionModel().getSelectedItem();
+        this.studentInfoCopy = student;
+        tfFirstname.setText(student.getFirstname());
+        tfMiddleName.setText(student.getMiddlename());
+        tfLastname.setText(student.getLastname());
+
+        // enable update & delete buttons
+        enableDelAndUpdateBtn();
+        btnSave.setDisable(true);
+    }
+
+    private void onUpdateStudent(ActionEvent actionEvent){
+        if(studentInfoCopy != null){
+            String firstname = tfFirstname.getText() != null ? tfFirstname.getText().trim() : "";
+            String middleName = tfMiddleName.getText() != null ? tfMiddleName.getText().trim() : "";
+            String lastname = tfLastname.getText() != null ? tfLastname.getText().trim(): "";
+
+            // ensure fields are not empty
+            if(noInput("First Name", firstname)){ return;}
+            if(noInput("Middle Name", middleName)){ return;}
+            if(noInput("Last Name", lastname)){ return;}
+
+            Student student = new Student(studentInfoCopy.getUid(), firstname, middleName, lastname);
+            AppQuery aq = new AppQuery();
+            final boolean updated = aq.updatedStudent(student);
+            if(updated){
+                toastAlert("Success", "Updated Successfully!");
+            }else {
+                toastAlert("Error", "Something Went Wrong During Update Process!");
+            }
+
+            emptyFields();
+            this.studentInfoCopy = null;
+
+            // refresh table list
+            initTableData();
+            disableDelAndUpdateBtn();
+        }else{
+            toastAlert("Error", "Record Id Missing!");
+        }
+    }
+
+    private void onDeleteStudent(ActionEvent actionEvent){
+        if(studentInfoCopy != null){
+            AppQuery aq = new AppQuery();
+            final boolean deleted = aq.deleteStudent(studentInfoCopy.getUid());
+            if(deleted){
+                toastAlert("Success", "Deleted Successfully!");
+            }else {
+                toastAlert("Error", "Something Went Wrong During Delete Process!");
+            }
+
+            emptyFields();
+            this.studentInfoCopy = null;
+
+            // refresh table list
+            initTableData();
+
+            // disable delete and update buttons
+            disableDelAndUpdateBtn();
+        }else{
+            toastAlert("Error", "Record Id Missing!");
+        }
+    }
+
+    public void disableNewAndSaveBtn(){
+        btnNew.setDisable(true);
+        btnSave.setDisable(true);
+    }
+
+    public void enableNewAndSaveBtn(){
+        btnNew.setDisable(false);
+        btnSave.setDisable(false);
+    }
+
+    private void disableDelAndUpdateBtn(){
+        btnUpdate.setDisable(true);
+        btnDelete.setDisable(true);
+    }
+
+    private void enableDelAndUpdateBtn(){
+        btnUpdate.setDisable(false);
+        btnDelete.setDisable(false);
     }
 }
--- src/main/java/com/example/fxmysqlcrud/data/AppQuery.java
+++ src/main/java/com/example/fxmysqlcrud/data/AppQuery.java
@@ -1,7 +1,6 @@
 package com.example.fxmysqlcrud.data;
 
 import com.example.fxmysqlcrud.models.Student;
-import com.example.fxmysqlcrud.data.DBConnection;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -9,18 +8,18 @@ import java.sql.*;
 
 public class AppQuery {
 
-    private final DBConnection dbConnection = new DBConnection();
+    private final DBConnection dbConnDriver = new DBConnection();
     public void addStudent(Student student){
 
         try {
-            dbConnection.createDBConn();
-            PreparedStatement ps = dbConnection.getConn().prepareStatement("INSERT INTO students(firstname, middlename, lastname) VALUES(?, ?, ?)");
+            dbConnDriver.createDBConn();
+            PreparedStatement ps = dbConnDriver.getConn().prepareStatement("INSERT INTO students(firstname, middlename, lastname) VALUES(?, ?, ?)");
             ps.setString(1, student.getFirstname());
             ps.setString(2, student.getMiddlename());
             ps.setString(3, student.getLastname());
             ps.execute();
             ps.close();
-            dbConnection.closeDBConn();
+            dbConnDriver.closeDBConn();
         }catch (SQLException e){
             System.out.println(e.getMessage());
         }
@@ -30,10 +29,10 @@ public class AppQuery {
         ObservableList<Student> studentList = FXCollections.observableArrayList();
         ResultSet resultSet;
         Statement st;
-        dbConnection.createDBConn();
-        Connection conn = dbConnection.getConn();
+        dbConnDriver.createDBConn();
+        Connection conn = dbConnDriver.getConn();
         try {
-            String query = "SELECT uid, firstname, middlename, lastname FROM students ORDER BY firstname ASC";
+            String query = "SELECT uid, firstname, middlename, lastname FROM students ORDER BY uid ASC";
             st = conn.createStatement();
             resultSet = st.executeQuery(query);
             while (resultSet.next()){
@@ -50,6 +49,57 @@ public class AppQuery {
         }
 
         return studentList;
+    }
+
+    public boolean updatedStudent(Student student){
+        boolean updated = false;
+        PreparedStatement ps;
+        dbConnDriver.createDBConn();
+
+        try {
+            ps = dbConnDriver.getConn().prepareStatement("UPDATE `students`\n" +
+                    "SET\n" +
+                    "`firstname` = ?,\n" +
+                    "`middlename` = ?,\n" +
+                    "`lastname` = ?\n" +
+                    "WHERE `uid` = ?;\n");
+
+            ps.setString(1, student.getFirstname());
+            ps.setString(2, student.getMiddlename());
+            ps.setString(3, student.getLastname());
+            ps.setInt(4, student.getUid());
+            ps.execute();
+
+            ps.close();
+            dbConnDriver.closeDBConn();
+
+            updated = true;
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+
+        return updated;
+    }
+
+    public boolean deleteStudent(Integer uid){
+        boolean deleted = false;
+        PreparedStatement ps;
+        dbConnDriver.createDBConn();
+
+        try {
+            ps = dbConnDriver.getConn().prepareStatement("DELETE FROM `students`\n" +
+                    "WHERE uid = ?;\n");
+            ps.setInt(1, uid);
+            ps.execute();
+
+            ps.close();
+            dbConnDriver.closeDBConn();
+
+            deleted = true;
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return deleted;
     }
 
 }
